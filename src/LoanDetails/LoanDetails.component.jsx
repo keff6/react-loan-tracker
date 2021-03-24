bbbc485bc7d6c6001833730d95dd5ce476885f8e
keff6 <kev.fallas@gmail.com>
2021-03-23 18:25:52 -0600

    Add Loan Details page placeholder

--- src/LoanDetails/LoanDetails.component.jsx
+++ src/LoanDetails/LoanDetails.component.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import Header from '../Header/Header.component'
 
-const LoanDetails = () => <div>Test</div>
+const LoanDetails = () => (
+  <>
+    <Header title="My Loans / Loan Details"/>
+    <h1>TODO: Add details</h1>
+  </>
+)
 
 export default LoanDetails;
